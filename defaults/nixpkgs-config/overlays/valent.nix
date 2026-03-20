53bfc37050e81cb6ef8305375f5b869a85085c9c
Corey O'Connor <coreyoconnor@gmail.com>
2026-03-20 06:04:50 +0000

    updates

--- defaults/nixpkgs-config/overlays/valent.nix
+++ defaults/nixpkgs-config/overlays/valent.nix
@@ -6,9 +6,11 @@ self: super: {
       # rev = "ef1945c69993e8614b1a8444461d72e13652aab6";
       # hash = "sha256-pgL8LzEp6+kKLDUPzkY1ehg+4N7zGSff7kThNcaCsw0=";
       owner = "andyholmes";
-      rev = "d880881d6e78f8b19159effdd619d693467d0061";
-      hash = "sha256-rmhSpUptLJfPSnDUUvf6eVMl+KVPOTOTpF4ySr57BHc=";
+      rev = "df82168bc37ad1ec700c66b0f0f5dfd7a07be485";
+      hash = "sha256-bg5p7Juw+O2vrfi2uDA69NPy68Zu8ig4ycVjhGkQ4ps=";
       fetchSubmodules = true;
     };
+
+    buildInputs = oldAttrs.buildInputs ++ [ self.libdex ];
   });
 }
