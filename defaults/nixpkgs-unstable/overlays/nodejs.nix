fb2d83897f46dcfff8d57c835395e8f725ec02a0
Corey O'Connor <coreyoconnor@gmail.com>
2026-07-13 16:00:51 +0000

    try tracing these to see why they have no effect

--- defaults/nixpkgs-unstable/overlays/nodejs.nix
+++ defaults/nixpkgs-unstable/overlays/nodejs.nix
@@ -1,12 +1,15 @@
-self: super: {
-  nodejs-slim_24 = super.nodejs-slim_24.overrideAttrs {
+self: super: rec {
+  nodejs-slim_24 = builtins.trace "unstable nodejs-slim_24" (super.nodejs-slim_24.overrideAttrs {
     doCheck = false;
+    checkTarget = [];
     checkPhase = ''
       echo skip
     '';
-  };
+  });
 
   nodejs_24 = super.nodejs_24.override {
-    nodejs-slim = self.nodejs-slim_24;
+    nodejs-slim = nodejs-slim_24;
   };
+
+  nodejs = nodejs_24;
 }
